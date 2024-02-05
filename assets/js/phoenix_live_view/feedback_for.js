99090104d1c7e2dbebb8e1235d6f69c50ca8f4b4
Steffen Deusch <steffen@deusch.me>
2024-02-05 18:22:09 +0100

    skip elements that are not in the DOM any more
    remove no-feedback class when phx-feedback-for is removed

--- assets/js/phoenix_live_view/feedback_for.js
+++ assets/js/phoenix_live_view/feedback_for.js
@@ -36,7 +36,15 @@ const FeedbackFor = {
     // when a container is updated
     const feedbackResults = {}
     feedbackContainers.forEach(el => {
+      // skip elements that are not in the DOM
+      if(!document.contains(el)) return
       const feedback = el.getAttribute(PHX_FEEDBACK_FOR)
+      if(!feedback){
+        // the container previously had phx-feedback-for, but now it doesn't
+        // remove the class from the container (if it exists)
+        JS.addOrRemoveClasses(el, [], [PHX_NO_FEEDBACK_CLASS])
+        return
+      }
       if(feedbackResults[feedback] === true){
         this.hideFeedback(el)
         return
